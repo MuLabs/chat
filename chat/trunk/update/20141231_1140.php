339456517cc47d2e5f330f5e333db12bb176e1cb
rschiano <rschiano@2091705c-a81e-4269-9311-d9c6a0b36376>
2015-04-17 09:34:16 +0000

    Remove old updates

--- chat/trunk/update/20141231_1140.php
+++
@@ -1,7 +0,0 @@
-<?php
-$sql = "
-ALTER TABLE `chatMessage`
-CHANGE COLUMN `date_insert` `dateInsert` DATETIME NOT NULL ,
-CHANGE COLUMN `date_edit` `dateEdit` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ;
-";
-$handler->query($sql);
--- chat/trunk/update/20150108_1755.php
+++
@@ -1,9 +0,0 @@
-<?php
-$sql = "
-CREATE TABLE `chatMessageUser` (
-  `idUser` INT UNSIGNED NOT NULL,
-  `idChatMessage` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`idUser`)
-) ENGINE=MEMORY;
-";
-$handler->query($sql);
